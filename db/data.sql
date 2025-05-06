15119a14ab70ad7bfc1c9111521522f47aeefd40
clint <clintjackson601@gmail.com>
2025-05-06 11:35:04 -0500

    did insert

---
+++ db/data.sql
@@ -0,0 +1,132 @@
+insert into format(format)
+values('physical');
+
+insert into format(format)
+values
+('e-book'),
+('audio');
+
+
+insert into qty(qty)
+values 
+('1, 20'),
+('2, 10'),
+('3, 12'),
+('4, 18'),
+('5, 45'),
+('6, 22'),
+('7, 14'),
+('8, 20'),
+('9 ,11'),
+('10, 10'),
+('11, 27'),
+('12, 14'),
+('13, 28'),
+('14, 16'),
+('15, 15');
+
+insert into genre(genre)
+values ('fantasy');
+
+insert into genre(genre)
+values
+('superhero'),
+('mystery'),
+('psychological thiller'),
+('horror'),
+('children literature'),
+('comedy'),
+('action'),
+('dark fantasy'),
+('adventure'),
+('supernaturnal'),
+('shonen'),
+('historical'),
+('martial arts');
+
+
+insert into publisher(publisher)
+values ('bloomsbury')
+
+
+insert into publisher(publisher)
+values
+('dc comics'),
+('shueisha'),
+('scholastic'),
+('random house'),
+('skybound entertainment'),
+('amulet books'),
+('hakusensha'),
+('naver webtoon'),
+('vic medium'),
+('harper and brothers');
+
+insert into author(author)
+values ('jk. rowling')
+
+
+insert into author(author)
+values
+('scott snyder'),
+('tsugumi ohba'),
+('r.l.stine'),
+('dr seuss'),
+('robert kirkman'),
+('jeff kinney'),
+('kentaro miura'),
+('lee jong'),
+('takehiko iuninoue'),
+('geoff johns'),
+('e.b. white');
+
+
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+)
+values
+('harry potter and the philosopher stone', 1, 1, 1997, 'lst edition', 1999,'hardback','english', 223);
+
+
+
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+)
+values
+('absolute batman',	2 , 2, 2024	'1st edition', 2024, 'paperback', 5, 'english', 176),
+('deathnote', 3,	3, 2003, '1st edition', 2003,' hardback', 5, 'japanense', 200),
+('goosebumps welcome to the deadhouse', 4, 4, 1992, '1st edition	1992',	'paperback', 5,	'english', 123),
+('cat in the hat', 5	, 5, 1957,'	1st edition	1957', 'hardback', 5, 'english', 61),
+('invincible', 6, 6,	2005, '4th edition', 2023,'paperback', 3,' english',144),
+('diary of the wimpy kid', 7,7, 2007, '1st edition', 2007, 'paperback' , 3, 'english' 244),
+('berserk', 8, 8	, 1986, '1st edition', 2003, 'paperback', 4, 'japanense' 224),
+('green eggs and ham', 4, 5,	1960, '1st edition', 1960,	'hardback',	5,	'english', 62),
+('tower of god',	9, 9, null,'2nd edition', 2023,	'e-book', 2, 'korean', 272),
+('ordeal', 10, 9, null, null, null, 'e-book', 3,	'english', 90),
+('harry potter and the prisoner of azkaban' 1, 1, 1999, '1st edition', 1999,	'hardback', 4,	'english'	435),
+('vagabond', 11,	10,	1998, '3rd edition', 2008,	'paperback', 3, 'japanense', 728),
+('absolute superman', 12	, 2, 2024, '1st edtion', 2024, 'hardback' 5, 'english',	664),
+("charlotte's web", 1952, '4th edition',	2001,	'hardback'	5,	'english '	184);
+
+
+
+
+
---
+++ db/schema.sql
@@ -0,0 +1,83 @@
+drop database if exists booksdb;
+create database booksdb;
+use booksdb;
+
+create table format(
+format_id tinyint unsigned auto_increment not null,
+format varchar(30) not null,
+data_created timestamp default current_timestamp,
+data_updated timestamp default current_timestamp on update current_timestamp,
+constraint pk_format primary key (format_id),
+);
+
+
+
+create table qty (
+    qty_id tinyint unsigned auto_increment not null,
+    qty tinyint unsigned not null,
+    data_created timestamp default current_timestamp,
+    data_updated timestamp default current_timestamp on update current_timestamp,
+    constraint pk_qty primary key (qty_id),
+);
+
+
+create table genre (
+    genre_id tinyint unsigned auto_increment not null,
+    genre varchar(20) unsigned not null,
+    data_created timestamp default current_timestamp,
+    data_updated timestamp default current_timestamp on update current_timestamp,
+    constraint pk_genre primary key (genre_id),
+);
+
+
+create table publisher (
+    publisher_id tinyint unsigned auto_increment not null,
+    publisher varchar(30) unsigned not null,
+    data_created timestamp default current_timestamp,
+    data_updated timestamp default current_timestamp on update current_timestamp,
+    constraint pk_publisher primary key (publisher_id),
+);
+
+
+
+create table author (
+    author_id tinyint unsigned auto_increment not null,
+    author varchar(25) unsigned not null,
+    data_created timestamp default current_timestamp,
+    data_updated timestamp default current_timestamp on update current_timestamp,
+    constraint pk_author primary key (author_id),
+);
+
+
+
+create table book (
+    book_id smallint unsigned auto_increment not null,
+    title varchar(100) unsigned not null,
+    publisher_id tinyint unsigned not null,
+    copyright year,
+    edition enum ('1st editon', '2nd editon', '3rd editon', '4th editon') default  '1st editon',
+    edition_year year,
+    binding enum('paperback', 'hardback','e-book') default 'paperback',
+    rating enum('1', '2', '3', '4', '5') default '1',
+    language enum ('english','japanense','korean') default 'english',
+    num_pages smallint not null,
+    data_created timestamp default current_timestamp,
+    data_updated timestamp default current_timestamp on update current_timestamp,
+    constraint pk_book primary key (book_id),
+    constraint fk_author foreign key (author_id)
+    references publisher (publisher_id),
+    constraint fk_publisher foreign key (publisher_id)
+    references author (author_id),
+);
+
+
+create table book_to_genre(
+    book_id smallint unsigned not null,
+    genre_id smallint unsigned not null,
+    data_created timestamp default current_timestamp,
+    data_updated timestamp default current_timestamp on update current_timestamp,
+    constraint fk_book_bg foreign key (book_id)
+    references book (book_id),
+    constraint fk_genre_bg foreign key (genre_id)
+    references genre (genre_id)
+);
