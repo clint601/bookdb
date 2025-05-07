e8bc786cf9e2438b3c8776a00681caf71e5dcb3f
clint <clintjackson601@gmail.com>
2025-05-07 11:33:09 -0500

    done

--- db/data.sql
+++ db/data.sql
@@ -1,55 +1,58 @@
-insert into format(format)
-values('physical');
+INSERT INTO format (format)
+values ('physical');
 
-insert into format(format)
+insert into format (format)
 values
 ('e-book'),
 ('audio');
 
-
-insert into qty(qty)
-values 
-('1, 20'),
-('2, 10'),
-('3, 12'),
-('4, 18'),
-('5, 45'),
-('6, 22'),
-('7, 14'),
-('8, 20'),
-('9 ,11'),
-('10, 10'),
-('11, 27'),
-('12, 14'),
-('13, 28'),
-('14, 16'),
-('15, 15');
-
-insert into genre(genre)
+insert into genre (genre)
 values ('fantasy');
 
-insert into genre(genre)
+insert into genre (genre)
 values
-('superhero'),
-('mystery'),
-('psychological thiller'),
-('horror'),
-('children literature'),
-('comedy'),
-('action'),
-('dark fantasy'),
-('adventure'),
-('supernaturnal'),
-('shonen'),
-('historical'),
+('superhero');
+insert into genre (genre)
+values
+('mystery');
+insert into genre (genre)
+values
+('psychological thriller');
+insert into genre (genre)
+values
+('horror');
+insert into genre (genre)
+values
+('children literature');
+insert into genre (genre)
+values
+('comedy');
+insert into genre (genre)
+values
+('action');
+insert into genre (genre)
+values
+('dark fantasy');
+insert into genre (genre)
+values
+('adventure');
+insert into genre (genre)
+values
+('supernatural');
+insert into genre (genre)
+values
+('shonen');
+insert into genre (genre)
+values
+('historical');
+insert into genre (genre)
+values
 ('martial arts');
 
+insert into publisher (publisher)
+values ('bloomsbury');
 
-insert into publisher(publisher)
-values ('bloomsbury')
-
-
-insert into publisher(publisher)
+insert into publisher (publisher)
 values
 ('dc comics'),
 ('shueisha'),
@@ -62,27 +65,28 @@ values
 ('vic medium'),
 ('harper and brothers');
 
-insert into author(author)
-values ('jk. rowling')
+insert into author (author)
+values ('jk. rowling');
 
-
-insert into author(author)
+insert into author (author)
 values
 ('scott snyder'),
 ('tsugumi ohba'),
-('r.l.stine'),
+('r.l stine'),
 ('dr seuss'),
 ('robert kirkman'),
 ('jeff kinney'),
 ('kentaro miura'),
 ('lee jong'),
-('takehiko iuninoue'),
+('takekiko iuninoue'),
 ('geoff johns'),
 ('e.b. white');
+insert into author (author)
+values
+('brent bristol');
 
 insert into book(
     title,
-    author,
     author_id,
     publisher_id,
     copyright_year,
@@ -90,12 +94,12 @@ insert into book(
     edition_year,
     binding,
     rating,
-    language
+    language,
     num_pages,
     qty,
-    cover_image,
+    cover_image
 )
-values(
+values (
     "harry potter and the philosopher's stone",
     1,
     1,
@@ -110,64 +114,248 @@ values(
     'harry_porter.jpeg'
 );
 
-
 insert into book(
     title,
     author_id,
     publisher_id,
-    copyright
+    copyright_year,
     edition,
     edition_year,
     binding,
     rating,
     language,
     num_pages,
-    qty,
+    qty, 
     cover_image
 )
 values
-('absolute batman',	2 , 2, 2024, '1st edition', 2024, 'paperback', 5, 'english', 176, 10, 'batman.jpeg'),
-('deathnote', 3, 3, 2003, '1st edition', 2003,' hardback', 5, 'japanese', 200, 12, 'deathnote.jpeg'),
-('goosebumps welcome to the deadhouse', 4, 4, 1992, '1st edition', 1992, 'paperback', 5, 'english', 123, 18, 'goosebumps.jpeg'),
-('cat in the hat', 5, 5, 1957, '1st edition', 1957, 'hardback', 5, 'english', 61, 45, 'cat.jpeg'),
-('invincible', 6, 6, 2005, '4th edition', 2023,'paperback', 3,' english', 144, 22, 'invincible.jpeg'),
-('diary of the wimpy kid', 7, 7, 2007, '1st edition', 2007, 'paperback' , 3, 'english' 244, 14, 'wimpykid.jpeg'),
-('berserk', 8, 8, 1986, '1st edition', 2003, 'paperback', 4, 'japanese', 224, 20, 'berserk.jpeg'),
-('green eggs and ham', 4, 5, 1960, '1st edition', 1960,	'hardback',	5,	'english', 62, 11, 'greeneggs.jpeg'),
-('tower of god', 9, 9, null,'2nd edition', 2023, 'e-book', 2, 'korean', 272, 8, 'towergod.jpeg'),
-('ordeal', 10, 9, null, null, null, 'e-book', 3,'english', 90, 11, 10, 'ordeal.jpeg'),
-('harry potter and the prisoner of azkaban' 1, 1, 1999, '1st edition', 1999,	'hardback', 4,	'english'	435,  27, 'harry_porter2.jpeg')
-('vagabond', 11, 10, 1998, '3rd edition', 2008,	'paperback', 3, 'japanese', 728, 14, 'vagabond.jpeg'),
-('absolute superman', 12, 2, 2024, '1st edition', 2024, 'hardback' 5, 'english', 664, 28, 'superman.jpeg'),
-("charlotte's web", 1952, '4th edition', 2001, 'hardback' 5, 'english', 184, 16, "charlotte's_web.jpeg");
-
-
-
-
-
-insert into book_to_genre(book_id, genre_id)
+('absolute batman',	2, 2, 2024, '1st edition', 2024, 'paperback', 5, 'english', 176, 10, 'batman.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
 values
-(1, 10), (1,1)
+('deathnote', 3, 3, 2003, '1st edition', 2003,'hardback', 5, 'japanese', 200, 12, 'deathnote.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('goosebumps welcome to the deadhouse', 4, 4, 1992, '1st edition', 1992, 'paperback', 5, 'english', 123, 18, 'goosebumps.jpeg');
 
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('cat in the hat', 5, 5, 1957, '1st edition', 1957, 'hardback', 5, 'english', 61, 45, 'cat.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('invincible', 6, 6, 2005, '4th edition', 2023,'paperback', 3, 'english', 144, 22, 'invincible.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('diary of the wimpy kid', 7, 7, 2007, '1st edition', 2007, 'paperback', 3, 'english', 244, 14, 'wimpykid.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('berserk', 8, 8, 1986, '1st edition', 2003, 'paperback', 4, 'japanese', 224, 20, 'berserk.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('green eggs and ham', 5, 5, 1960, '1st edition', 1960,	'hardback',	5,	'english', 62, 11, 'greeneggs.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('tower of god', 9, 9, null,'2nd edition', 2023, 'e-book', 2, 'korean', 272, 8, 'towergod.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('ordeal', 13, 9, 2021, '1st edition', 2021, 'e-book', 3, 'english', 90, 11, 'ordeal.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('harry potter and the prisoner of azkaban', 1, 1, 1999, '1st edition', 1999, 'hardback', 4,'english', 435, 27, 'harry_porter2.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('vagabond', 10, 10, 1998, '3rd edition', 2008,	'paperback', 3, 'japanese', 728, 14, 'vagabond.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('absolute superman', 11, 2, 2024, '1st edition', 2024, 'hardback', 5, 'english', 664, 28, 'superman.jpeg');
+insert into book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+("charlotte's web", 12, 11, 1952, '4th edition', 2001, 'hardback',  5, 'english', 184, 16, "charlotte's_web.jpeg");
 
+insert into book_to_genre (book_id, genre_id)
+values (1, 1), (1, 10);
 
-insert into book_to_genre(book_id, genre_id)
+insert into book_to_genre (book_id, genre_id)
 values
 (2, 2), (2, 8),
-(3, 3 ), (3, 4),
-(4, 5), (4, 6),
-(5, 7), (5,6),
+(3,3), (3, 4),
+(4, 5 ), (4, 6),
+(5, 6),
 (6, 2), (6, 8),
-(7, 6), (7,7),
-(8, 9), (8, 10), (8, 5), (8, 8),
+(7, 6), (7, 7),
+(8, 5), (8, 9), (8, 8), (8, 10),
 (9, 6),
-(10, 1), (10, 8),(10, 10) (10, 14),
-(11, 2), (11, 8), (11, 14) (11, 12),
+(10, 1), (10, 8), (10, 10), (10, 14),
+(11, 2), (11, 8), (11, 14), (11, 12),
 (12, 1), (12, 10),
-(13, 14), (13, 13), (13, 12),
+(13, 13), (13, 14), (13, 12),
 (14, 2), (14, 8),
-(15, 6)
-;
-
-
-
+(15, 6);
--- db/schema.sql
+++ db/schema.sql
@@ -1,75 +1,70 @@
-drop database if exists booksdb;
-create database booksdb;
-use booksdb;
+DROP DATABASE IF EXISTS librarydb;
+create database librarydb;
+use librarydb;
 
-create table format(
-format_id tinyint unsigned auto_increment not null,
-format varchar(30) not null,
-data_created timestamp default current_timestamp,
-data_updated timestamp default current_timestamp on update current_timestamp,
-constraint pk_format primary key (format_id),
+create table format (
+    format_id tinyint unsigned auto_increment not null,
+    format varchar(20) not null,
+    date_created timestamp default current_timestamp,
+    last_update timestamp default current_timestamp on update current_timestamp,
+    constraint pk_format primary key (format_id)
 );
-
-
 
 create table genre (
     genre_id tinyint unsigned auto_increment not null,
-    genre varchar(60) unsigned not null,
-    data_created timestamp default current_timestamp,
-    data_updated timestamp default current_timestamp on update current_timestamp,
-    constraint pk_genre primary key (genre_id),
+    genre varchar(50),
+    date_created timestamp default current_timestamp,
+    last_update timestamp default current_timestamp on update current_timestamp,
+    constraint pk_genre primary key (genre_id)
 );
-
 
 create table publisher (
     publisher_id tinyint unsigned auto_increment not null,
-    publisher varchar(30) unsigned not null,
-    data_created timestamp default current_timestamp,
-    data_updated timestamp default current_timestamp on update current_timestamp,
-    constraint pk_publisher primary key (publisher_id),
+    publisher varchar(30),
+    date_created timestamp default current_timestamp,
+    last_update timestamp default current_timestamp on update current_timestamp,
+    constraint pk_publisher primary key (publisher_id)
 );
-
-
 
 create table author (
     author_id tinyint unsigned auto_increment not null,
-    author varchar(25) unsigned not null,
-    data_created timestamp default current_timestamp,
-    data_updated timestamp default current_timestamp on update current_timestamp,
-    constraint pk_author primary key (author_id),
+    author varchar(20),
+    date_created timestamp default current_timestamp,
+    last_update timestamp default current_timestamp on update current_timestamp,
+    constraint pk_author primary key (author_id)
 );
-
-
 
 create table book (
     book_id smallint unsigned auto_increment not null,
-    title varchar(100) unsigned not null,
+    title varchar(100) not null,
+    author_id tinyint unsigned not null,
     publisher_id tinyint unsigned not null,
-    copyright year,
-    edition enum ('1st editon', '2nd editon', '3rd editon', '4th editon') default  '1st editon',
+    copyright_year year,
+    edition enum ('1st edition', '2nd edition', '3rd edition', '4th edition') default '1st edition',
     edition_year year,
-    binding enum('paperback', 'hardback','e-book') default 'paperback',
-    rating enum('1', '2', '3', '4', '5') default '1',
-    language enum ('english','japanense','korean') default 'english',
-    num_pages smallint not null,
-    qty tinyint unsigned not null
-    data_created timestamp default current_timestamp,
-    data_updated timestamp default current_timestamp on update current_timestamp,
+    binding enum ('paperback', 'hardback', 'e-book') default 'hardback',
+    rating enum ('1','2','3','4','5') default '1',
+    language enum  ('english', 'japanese', 'korean') default 'english',
+    num_pages smallint unsigned not null,
+    qty tinyint unsigned not null,
+    cover_image mediumtext,
+    date_created timestamp default current_timestamp,
+    last_update timestamp default current_timestamp on update current_timestamp,
     constraint pk_book primary key (book_id),
-    constraint fk_author foreign key (author_id)
-    references publisher (publisher_id),
-    constraint fk_publisher foreign key (publisher_id)
-    references author (author_id),
+    constraint fk_author foreign key (author_id) references author (author_id),
+    constraint fk_publisher foreign key (publisher_id) references publisher (publisher_id)
 );
 
-
-create table book_to_genre(
+create table book_to_genre (
     book_id smallint unsigned not null,
-    genre_id smallint unsigned not null,
-    data_created timestamp default current_timestamp,
-    data_updated timestamp default current_timestamp on update current_timestamp,
-    constraint fk_book_bg foreign key (book_id)
-    references book (book_id),
-    constraint fk_genre_bg foreign key (genre_id)
-    references genre (genre_id)
+    genre_id tinyint unsigned not null,
+    date_created timestamp default current_timestamp,
+    last_update timestamp default current_timestamp on update current_timestamp,
+    constraint fk_book_bg foreign key (book_id) references book (book_id),
+    constraint fk_genre_bg foreign key (genre_id) references genre (genre_id)
 );
+
+-- resetting auto increment
+set @num:= 0;
+update my_table set id = @num:= (@num + 1);
+alter table my_table auto_increment = 1;
